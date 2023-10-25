d5bd36c4069a2fcd15ec5e4fb4908119d8f39b47
Jacob-Allebach <allebach01@allegheny.edu>
2023-10-25 00:07:56 -0400

    Delete old work and fix ServerRpc calls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,25 +7,28 @@ public class PlayerController : NetworkBehaviour
 {
 
     public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    public NetworkVariable<bool> host = new NetworkVariable<bool>(false);
+
+    private void Start()
+    {
+        if (NetworkManager.Singleton.IsHost) NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject().GetComponent<PlayerController>().host.Value = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(0, 0, 0);
-
-        if (Input.GetKey(KeyCode.W)) movement.z = +1f;
-        if (Input.GetKey(KeyCode.S)) movement.z = -1f;
-        if (Input.GetKey(KeyCode.A)) movement.x = -1f;
-        if (Input.GetKey(KeyCode.D)) movement.x = +1f;
-
-        float moveSpeed = 3f;
-
-        if (NetworkManager.Singleton.IsHost)
+        if (IsOwner == true)
         {
-            Position.Value += movement * moveSpeed * Time.deltaTime;
-        }
-        else
-        {
+
+            Vector3 movement = new Vector3(0, 0, 0);
+
+            if (Input.GetKey(KeyCode.W)) movement.z = +1f;
+            if (Input.GetKey(KeyCode.S)) movement.z = -1f;
+            if (Input.GetKey(KeyCode.A)) movement.x = -1f;
+            if (Input.GetKey(KeyCode.D)) movement.x = +1f;
+
+            float moveSpeed = 3f;
+
             MovementServerRpc(movement, moveSpeed);
         }
 
@@ -36,5 +39,11 @@ public class PlayerController : NetworkBehaviour
     void MovementServerRpc(Vector3 movement, float moveSpeed, ServerRpcParams rpcParams = default)
     {
         Position.Value += movement * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    void SetHostServerRpc()
+    {
+        host.Value = true;
     }
 }
